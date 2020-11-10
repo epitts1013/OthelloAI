62a03233822347e263d3b9e68a2698766b2d6105
Eric Pitts <epitts1013@gmail.com>
2020-11-09 22:32:52 -0600

    Working on CheckMove(), holy shit this sucks ass

---
+++ Othello/MoveTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Othello
+{
+    class MoveTree
+    {
+        // class for nodes in tree
+        public class TreeNode
+        {
+            // stores reference to parent node
+            public TreeNode Parent { get; private set; }
+
+            // stores references to child nodes
+            public List<TreeNode> Children { get; private set; }
+
+            // stores ply that tree node is at
+            public int Ply { get; set; }
+
+            // default constructor
+            public TreeNode(TreeNode parent)
+            {
+                Parent = parent;
+                Children = new List<TreeNode>();
+                Ply = parent.Ply + 1;
+            }
+
+            // add a child to the node
+            public void AddChild(TreeNode newNode)
+            {
+                Children.Add(newNode);
+            }
+        }
+
+        // store reference to root node of tree
+        public TreeNode Root { get; private set; }
+
+        // default constructor
+        public MoveTree()
+        {
+            Root = new TreeNode(null);
+        }
+
+        // TODO: Finish this class
+    }
+}
--- Othello/OthelloGame.cs
+++ Othello/OthelloGame.cs
@@ -12,11 +12,14 @@ namespace Othello
         public char[,] BoardState { get; private set; }
 
         // bool tracks who's turn it is
-        public bool IsWhitesTurn { get; private set; }
+        public bool IsBlacksTurn { get; private set; }
 
+        // default constructor
         public OthelloGame()
         {
+            // initialize variables
             BoardState = new char[8,8];
+            IsBlacksTurn = true;
             
             // initialize all places to 'O', representing no piece
             for (int i = 0; i < 8; i++)
@@ -55,12 +58,13 @@ namespace Othello
                 return false;
             }
 
-            if (IsLegalMove(move))
+            List<int[]> positionsToUpdate;
+            if ((positionsToUpdate = CheckMove(moveArray)) != null)
             {
                 // TODO: Change board state based on move
 
                 // toggle current player turn
-                IsWhitesTurn = !IsWhitesTurn;
+                IsBlacksTurn = !IsBlacksTurn;
                 return true;
             }
             else
@@ -70,10 +74,71 @@ namespace Othello
             }
         }
 
-        public bool IsLegalMove(string move)
+        // returns a list positions to flip from a given move, or null if the move is invalid
+        public List<int[]> CheckMove(char[] move)
         {
-            // TODO: Implement IsValidMove()
-            throw new NotImplementedException();
+            // list of size 2 int arrays containing indexes of positions to flip color for
+            List<int[]> positionsToUpdate = new List<int[]>();
+
+            // get integer representation of move
+            int column = char.ToUpper(move[0]) - 65;
+            int row = move[1] - 49;
+
+            // color of current turn
+            char turnColor = IsBlacksTurn ? 'B' : 'W';
+
+            // counts how many tiles will be flipped
+            int count = 0;
+
+            // check upwards
+            for (int i = row; i >= 0; i--)
+            {
+                if (BoardState[column, i] == 'O') // if the search encounters a blank spot
+                    break;
+                else if (BoardState[column, i] != turnColor) // if the tile matches the opponent players tile color
+                    count++;
+                else if (BoardState[column, i] == turnColor) // if the tile matches the current players tile color
+                {
+                    // add positions if there are positions to add
+                    if (count != 0)
+                    {
+                        // go back and add positions between the two tiles
+                        for (int j = i + 1; j < row; j++)
+                            positionsToUpdate.Add(new int[] { column, j });
+                        break;
+                    }
+                    else break;
+                }
+            }
+            count = 0;
+
+            // check downwards
+            for (int i = row; i < 8; i++)
+            {
+                if (BoardState[column, i] == 'O') // if the search encounters a blank spot
+                    break;
+                else if (BoardState[column, i] != turnColor) // if the tile matches the opponent players tile color
+                    count++;
+                else if (BoardState[column, i] == turnColor) // if the tile matches the current players tile color
+                {
+                    // add positions if there are positions to add
+                    if (count != 0)
+                    {
+                        // go back and add positions between the two tiles
+                        for (int j = i - 1; j >= row; j--)
+                            positionsToUpdate.Add(new int[] { column, j });
+                        break;
+                    }
+                    else break;
+                }
+            }
+            count = 0;
+
+            // if list of positions to update isn't empty, return it, otherwise return null
+            if (positionsToUpdate.Count > 0)
+                return positionsToUpdate;
+            else
+                return null;
         }
 
         public string[] GetLegalMoves()
